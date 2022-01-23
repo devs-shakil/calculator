dc9b4c129a21a7fcf22c32806df0ca494e614e30
shakilwebmaster <mdshakil102036@gmail.com>
2022-01-23 12:42:16 +0600

    Calculator design and development

---
+++ js/calculate.js
@@ -0,0 +1,16 @@
+let operators = ["+", "-", "*", "/"];
+
+let output = null;
+let operator = null;
+let equal = null;
+let dot = null;
+
+function button_numbers(button){
+    operator = document.querySelector('.operator');
+    output = document.querySelector('.output');
+    equal = document.querySelector('.equal');
+    dot = document.querySelector('.dot');
+
+    
+}
+console.log(button_numbers());
